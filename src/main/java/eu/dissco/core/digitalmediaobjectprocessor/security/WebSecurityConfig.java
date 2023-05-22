d42f30a61a8af2922fb78196197883f413611a0d
southeo <soulaine.theo@gmail.com>
2023-05-22 13:34:08 +0200

    Code Review

--- src/main/java/eu/dissco/core/digitalmediaobjectprocessor/security/WebSecurityConfig.java
+++ src/main/java/eu/dissco/core/digitalmediaobjectprocessor/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointR
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,13 +18,17 @@ public class WebSecurityConfig {
   private final JwtAuthConverter jwtAuthConverter;
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests()
+    http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+            .requestMatchers(HttpMethod.GET, "**").permitAll()
             .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-            .anyRequest().authenticated();
-    http.oauth2ResourceServer()
-            .jwt()
-            .jwtAuthenticationConverter(jwtAuthConverter);
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            .anyRequest().authenticated());
+
+    http.oauth2ResourceServer(jwtOauth2ResourceServer -> jwtOauth2ResourceServer.jwt((
+            jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)
+    )));
+
+    http.sessionManagement(sessionManagement -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
   }
 
