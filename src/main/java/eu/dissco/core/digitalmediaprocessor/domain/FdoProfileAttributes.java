b29f7454d986493b743ab41a3749de0f68765b4d
southeo <soulaine.theo@gmail.com>
2024-10-10 15:03:53 +0200

    fdo profile fix

--- src/main/java/eu/dissco/core/digitalmediaprocessor/domain/FdoProfileAttributes.java
+++ src/main/java/eu/dissco/core/digitalmediaprocessor/domain/FdoProfileAttributes.java
@@ -5,27 +5,24 @@ import lombok.Getter;
 @Getter
 public enum FdoProfileAttributes {
 
-  TYPE("type", null),
-  ISSUED_FOR_AGENT("issuedForAgent", null),
-  REFERENT_NAME("referentName", null),
-  PRIMARY_MEDIA_ID("primaryMediaId", null),
-  LINKED_DO_PID("linkedDigitalObjectPid", null),
-  MEDIA_HOST("mediaHost", null),
-  IS_DERIVED_FROM_SPECIMEN("isDerivedFromSpecimen", "true"),
-  LINKED_DO_TYPE("linkedDigitalObjectType", "digital specimen"),
-  PRIMARY_MO_ID_TYPE("primaryMediaObjectIdType", "Resolvable"),
-  PRIMARY_MO_ID_NAME("primaryMediaObjectIdName", "ac:accessUri"),
-  DCTERMS_TYPE("dcterms:type", "image"),
-  LICENSE_NAME("licenseName", ""),
-  RIGHTSHOLDER_PID_TYPE("rightsholderPidType", "Resolvable"),
-  MEDIA_FORMAT("dcterms:format", "image");
+  MEDIA_HOST("mediaHost"),
+  MEDIA_HOST_NAME("mediaHostName"),
+  LINKED_DO_PID("linkedDigitalObjectPid"),
+  LINKED_DO_TYPE("linkedDigitalObjectType"),
+  MEDIA_ID("primaryMediaId"),
+  MEDIA_ID_TYPE("primaryMediaIdType"),
+  MEDIA_ID_NAME("primaryMediaIdName"),
+  MEDIA_TYPE("mediaType"),
+  MIME_TYPE("mimeType"),
+  LICENSE_NAME("licenseName"),
+  LICENSE_ID("licenseId"),
+  RIGHTS_HOLDER_ID("rightsHolderId"),
+  RIGHTS_HOLDER_NAME("rightsHolderName");
 
   private final String attribute;
-  private final String defaultValue;
 
-  FdoProfileAttributes(String attribute, String defaultValue) {
+  FdoProfileAttributes(String attribute) {
     this.attribute = attribute;
-    this.defaultValue = defaultValue;
   }
 
 }
